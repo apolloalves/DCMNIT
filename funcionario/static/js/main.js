8578ade6d5967f42ddd05b522fb40c76e7596da9
Apollo <apolloapolloalves@gmail.com>
2020-04-26 14:34:01 -0300

    Included Dialog Confirm - remove

--- funcionario/static/js/main.js
+++ funcionario/static/js/main.js
@@ -1,14 +1,18 @@
 'use-strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-    let elems = document.querySelectorAll( '.tooltipped' );
+    let tooltipped = document.querySelectorAll( '.tooltipped' );
     let sidenav = document.querySelectorAll( '.sidenav' );
-    let instances = M.Tooltip.init( elems,{enterDelay: 300,exitDelay:100} );
-    let instance = M.Sidenav.init( sidenav );
+    
+    let inst_tooltipped = M.Tooltip.init( tooltipped,{enterDelay: 300,exitDelay:100} );
+    let inst_sidenav = M.Sidenav.init( sidenav );
 
     var modal = document.querySelectorAll('.modal');
     let inst_modal = M.Modal.init(modal);
+
+    var modal_delete = document.querySelector('#modal_delete');
+    let inst_modal_delete = M.Modal.init(modal_delete);
+    
+    
    
-
 });
-
