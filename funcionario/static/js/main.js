0b6402e4ca61e83a40300b18bfb241d090a32bb1
apolloalves <apolloapolloalves@gmail.com>
2020-05-02 21:53:47 -0300

    Indent file

--- funcionario/static/js/main.js
+++ funcionario/static/js/main.js
@@ -1,18 +1,18 @@
 'use-strict'
+console.warn('Desenvolvido por DCMNIT - Soluções em Tecnologias')
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     let tooltipped = document.querySelectorAll( '.tooltipped' );
     let sidenav = document.querySelectorAll( '.sidenav' );
     
-    let inst_tooltipped = M.Tooltip.init( tooltipped,{enterDelay: 300,exitDelay:100} );
+    let inst_tooltipped = M.Tooltip.init( tooltipped,{ enterDelay: 300,exitDelay:100 } );
     let inst_sidenav = M.Sidenav.init( sidenav );
 
-    var modal = document.querySelectorAll('.modal');
+    let modal = document.querySelectorAll( '.modal' );
     let inst_modal = M.Modal.init(modal);
 
-    var modal_delete = document.querySelector('#modal_delete');
-    let inst_modal_delete = M.Modal.init(modal_delete);
-    
+    let modal_delete = document.querySelector( '#modal_delete' );
+    let inst_modal_delete = M.Modal.init( modal_delete );
     
    
 });
